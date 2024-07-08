63fe957f9c47c3c023c8e9438e54c931d5f6cbe0
danymcboockair <dsmmgc@gmail.com>
2024-07-08 21:04:33 +0100

    modifico en IntelliJ

--- HelloGit.java
+++ HelloGit.java
@@ -1,7 +1,7 @@
 public class HelloGit{
 
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello Git despues de subir a github");
         System.out.println("ahora que? volvemos?");
     }
 }
